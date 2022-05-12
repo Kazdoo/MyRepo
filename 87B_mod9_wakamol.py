80647f0416f55ca25b5e7855745f882a4a05a408
oged <olivier.ged@gmail.com>
2022-05-11 17:21:06 -0700

    iteration

--- 87B_mod9_wakamol.py
+++ 87B_mod9_wakamol.py
@@ -8,18 +8,18 @@ from pygame.sprite import *
 pygame.font.init()
 pygame.mixer.init()
 
-WIDTH = 1024
-HEIGHT = 768
+WIDTH = 900
+HEIGHT = 500
 WHITE = (255,255,255)
-WIN = display.set_mode ((WIDTH, HEIGHT)) # a surface
+WIN = pygame.display.set_mode ((WIDTH, HEIGHT)) # a surface
 SCORE_FONT = pygame.font.SysFont('comicsans', 40)
-WHACK_SOUND = pygame.mixer.Sound (os.path.join('Assets', 'whack.mp3'))
+#WHACK_SOUND = pygame.mixer.Sound (os.path.join('Assets', 'whack.mp3'))
 FPS = 60
 
 AVOCADO_HIT = pygame.USEREVENT +1
 
 
-display.set_caption("wakamole")
+display.set_caption("G-wakamole")
 
 
 class Mole (Sprite):
@@ -27,6 +27,14 @@ class Mole (Sprite):
         Sprite.__init__(self)
         self.image = image.load (os.path.join('Assets', 'avocado.png')).convert()
         self.rect = self.image.get_rect()
+
+    def flee (self):
+        self.rect.left = random.randint(0,600)
+        self.rect.top =random.randint(0,400)
+
+    def handle_click(self):
+        return
+
 
 
 class Banner (Sprite):
@@ -36,9 +44,7 @@ class Banner (Sprite):
         self.rect = self.image.get_rect().move(50,70)
 
 
-def flee (self):
-    self.rect.left = random.randint(0,600)
-    self.rect.top =random.randint(0,400)
+
 
 def update(self):
     self.rect = self.rect.move(3,0)
@@ -50,12 +56,22 @@ all_sprites = Group(my_mole1, my_mole2)
 
 
 
+def draw_window():
+    pygame.draw.rect(WIN, WHITE, )
+    #surface = pygame.display.set_mode (WIDTH, HEIGHT)
+
+    WIN.fill(WHITE)
+    all_sprites.draw(WHITE)
+    display.update()
+
 
 
 def main():
     #the event loop
 
     pygame.init()
+
+    score = 0
 
     clock = pygame.time.Clock()
     run = True
@@ -70,11 +86,12 @@ def main():
             if event.type == MOUSEBUTTONDOWN:
                 if my_mole1.rect.collidepoint (mouse.get_pos()):
                     my_mole1.flee()
+                    score += 1
+                    #SOUND.play()
                 
-                WIN.fill(WHITE)
-                all_sprites.draw(WHITE)
-        display.update()
 
+
+        draw_window()
 
 if __name__ == "__main__":
     main()
